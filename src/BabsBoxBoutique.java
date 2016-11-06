d5cb48c870e602596897c1cccb9d994313a2a71b
voidstar <voidstarr@users.noreply.github.com>
2016-11-06 11:10:44 -0800

    yee yee

--- src/BabsBoxBoutique.java
+++ src/BabsBoxBoutique.java
@@ -1,22 +1,31 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class BabsBoxBoutique {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = 0;
-		int stack = 0;
+		int stack = 1;
+		int[][] boxes = new int[n][3];
+		ArrayList<Integer> answers = new ArrayList<Integer>();
 		while ((n = Integer.parseInt(br.readLine())) != 0) {
 			String[] box;
-			int[][][] boxes = new int[stack][n][3];
 			for (int i = 0; i < n && (box = br.readLine().split(" ")) != null; i++) {
 				System.out.println(box);
-				boxes[stack][i][0] = Integer.parseInt(box[0]);
-				boxes[stack][i][1] = Integer.parseInt(box[1]);
-				boxes[stack][i][2] = Integer.parseInt(box[2]);
+				boxes[i][0] = Integer.parseInt(box[0]);
+				boxes[i][1] = Integer.parseInt(box[1]);
+				boxes[i][2] = Integer.parseInt(box[2]);
 			}
+
+			// TODO: logix; push answer to answers
+
 			stack++;
+		}
+
+		for (int i = 0; i < answers.size(); i++) {
+			System.out.println("Case " + (i + 1) + ": " + answers.get(i));
 		}
 
 	}
