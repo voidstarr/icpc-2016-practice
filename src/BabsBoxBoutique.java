cd46f30f902dd97cc09f80d8e6c9ec3d2f93b9bb
voidstar <voidstarr@users.noreply.github.com>
2016-11-06 11:00:42 -0800

    yee

--- src/BabsBoxBoutique.java
+++ src/BabsBoxBoutique.java
@@ -6,15 +6,17 @@ public class BabsBoxBoutique {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = 0;
+		int stack = 0;
 		while ((n = Integer.parseInt(br.readLine())) != 0) {
 			String[] box;
-			int[][] boxes = new int[n][3];
+			int[][][] boxes = new int[stack][n][3];
 			for (int i = 0; i < n && (box = br.readLine().split(" ")) != null; i++) {
 				System.out.println(box);
-				boxes[i][0] = Integer.parseInt(box[0]);
-				boxes[i][1] = Integer.parseInt(box[1]);
-				boxes[i][2] = Integer.parseInt(box[2]);
+				boxes[stack][i][0] = Integer.parseInt(box[0]);
+				boxes[stack][i][1] = Integer.parseInt(box[1]);
+				boxes[stack][i][2] = Integer.parseInt(box[2]);
 			}
+			stack++;
 		}
 
 	}
